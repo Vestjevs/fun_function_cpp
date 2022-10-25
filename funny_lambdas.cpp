6d32981b33f2422a3f0670f6d683e09c7e1adedb
Jevs <38396669+Vestjevs@users.noreply.github.com>
2022-10-25 16:22:41 +0300

    add different functions

--- funny_lambdas.cpp
+++ funny_lambdas.cpp
@@ -3,12 +3,36 @@
 
 namespace
 {
-  auto checkTime = [](void (*func)()) 
-  {
-      auto begin = std::chrono::high_resolution_clock::now();
-      func();
-      auto end = std::chrono::high_resolution_clock::now();
-      auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-      std::cout << "execution time : " << elapsed << std::endl;
-  };
+  // No return
+	auto check_time = [](void (*func)())
+	{
+		auto begin = std::chrono::high_resolution_clock::now();
+		func();
+		auto end = std::chrono::high_resolution_clock::now();
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+		std::cout << "execution time : " << elapsed << std::endl;
+	};
+
+	// sample with arg it can be edited by example
+	auto check_time_func_with_arg = [](void (*func)(int a), int a)
+	{
+		auto begin = std::chrono::high_resolution_clock::now();
+		func(a);
+		auto end = std::chrono::high_resolution_clock::now();
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+		std::cout << "execution time : " << elapsed << std::endl;
+	};
+
+	// sample with return it can be edited by example
+	auto checkTimeWithReturn = [](int (*func)()) -> int
+	{
+		auto begin = std::chrono::high_resolution_clock::now();
+		auto result = func();
+		auto end = std::chrono::high_resolution_clock::now();
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+		std::cout << "execution time : " << elapsed << std::endl;
+		return result;
+	};
+
+	// func <- pointer to function
 }
